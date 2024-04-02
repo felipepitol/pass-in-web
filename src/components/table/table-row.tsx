b91b51a5a05fae2325dc96fd7284af0179667d58
Felipe Pitol <felipepitol@gmail.com>
2024-04-02 18:25:30 -0300

    Fix interface TableRowProps para tr

--- src/components/table/table-row.tsx
+++ src/components/table/table-row.tsx
@@ -1,6 +1,6 @@
 import { ComponentProps } from 'react'
 
-interface TableRowProps extends ComponentProps<'th'> {}
+interface TableRowProps extends ComponentProps<'tr'> {}
 
 export function TableRow(props: TableRowProps) {
   return (
